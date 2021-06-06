a2fba1f6cf6938f4ac8a63c10eaf06d33ee22f27
ciprianjichici <ciprian.jichici@genisoft.eu>
2021-06-06 03:02:32 +0300

    Update resource group name pattern in setup

--- hands-on-labs/setup/automation/environment-setup.ps1
+++ hands-on-labs/setup/automation/environment-setup.ps1
@@ -20,7 +20,7 @@ if($IsCloudLabs){
         
         Connect-AzAccount -Credential $cred | Out-Null
 
-        $resourceGroupName = (Get-AzResourceGroup | Where-Object { $_.ResourceGroupName -like "*-L400*" }).ResourceGroupName
+        $resourceGroupName = (Get-AzResourceGroup | Where-Object { $_.ResourceGroupName -like "*synapse-*" }).ResourceGroupName
 
         if ($resourceGroupName.Count -gt 1)
         {
